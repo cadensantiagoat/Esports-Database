ce5762f22378897936b05f028776b89b66f8f33b
xfeng2023 <123416885+xfeng2023@users.noreply.github.com>
2026-04-15 23:56:20 -0700

    Add files via upload

--- esport_ddl.sql
+++ esport_ddl.sql
@@ -1,52 +1,111 @@
-DROP DATABASE IF EXISTS hw2;
-CREATE DATABASE IF NOT EXISTS hw2;
+DROP DATABASE IF EXISTS esport;
+CREATE DATABASE IF NOT EXISTS esport;
 DROP USER IF EXISTS 'feng'@'localhost';
 GRANT SELECT, INSERT, DELETE, UPDATE, EXECUTE ON hw2.* TO 'root'@'localhost';
 
-USE hw2;
+USE esport;
 
-CREATE TABLE TeamRoster (
-    ID INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    Name_First VARCHAR(100),
-    Name_Last VARCHAR(150) NOT NULL,
-    Street VARCHAR(250),
-    City VARCHAR(100),
-    State VARCHAR(100),
-    Country VARCHAR(100),
-    ZipCode CHAR(10),
-    CHECK (ZipCode REGEXP '^(?!0{5})(?!9{5})\\d{5}(-(?!0{4})(?!9{4})\\d{4})?$'),
+CREATE TABLE Users (
+    userID INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(100),
+    lastname VARCHAR(150) NOT NULL,
+    Email VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    role ENUM('player', 'coach', 'admin'),
     UNIQUE KEY uniq_name (Name_Last, Name_First),
     INDEX idx_last (Name_Last)
 );
 
-INSERT INTO TeamRoster
-VALUES (100, 'Donald', 'Duck', '1313 S. Harbor Blvd.', 'Anaheim', 'CA', 'USA', '92808-3232'),
-       (101, 'Daisy',  'Duck', '1180 Seven Seas Dr.', 'Lake Buena Vista', 'FL', 'USA', '32830'),
-       (107, 'Mickey', 'Mouse', '1313 S. Harbor Blvd.', 'Anaheim', 'CA', 'USA', '92808-3232'),
-       (111, 'Pluto',  'Dog', '1313 S. Harbor Blvd.', 'Anaheim', 'CA', 'USA', '92808-3232'),
-       (118, 'Scrooge', 'McDuck', '1180 Seven Seas Dr.', 'Lake Buena Vista', 'FL', 'USA', '32830'),
-       (119, 'Huebert(Huey)', 'Duck', '1110 Seven Seas Dr.', 'Lake Buena Vista', 'FL', 'USA', '32830'),
-       (123, 'Deuteronomy(Dewey)', 'Duck', '1110 Seven Seas Dr.', 'Lake Buena Vista', 'FL', 'USA', '32830'),
-       (128, 'Louie', 'Duck', '1110 Seven Seas Dr.', 'Lake Buena Vista', 'FL', 'USA', '32830'),
-       (129, 'Phooey', 'Duck', '101 Maihama Urayasu', 'Chiba Prefecture', 'Disney Tokyo', 'Japan', NULL),
-       (131, 'Della', 'Duck', '77700 Boulevard du Parc', 'Coupvary', 'Disney Paris', 'France', NULL);
+INSERT INTO Users
+VALUES (1000, 'Donald', 'Duck', 'donaldduck@gmail.com','123456','player'),
+       (1001, 'Daisy',  'Duck',  'daisyduck@gmail.com', '234567''coach'),
+       (1002, 'Mickey', 'Mouse', 'mickeymouse@gmail.com', '345678','admin'),
+       (1003, 'Louie', 'Duck', 'louieduck@gmail.com', '012345','player')
 
 
-CREATE TABLE Statistics (
-    ID INT(10) AUTO_INCREMENT PRIMARY KEY,
-    Player INT(10) UNSIGNED NOT NULL,
-    PlayerTimeMin TINYINT(2) UNSIGNED DEFAULT 0 CHECK (PlayerTimeMin BETWEEN 0 AND 40),
-    PlayerTimeSec TINYINT(2) UNSIGNED DEFAULT 0 CHECK (PlayerTImeSec BETWEEN 0 AND 59),
-    Points TINYINT(3) UNSIGNED DEFAULT 0,
-    Assists TINYINT(3) UNSIGNED DEFAULT 0,
-    Rebounds TINYINT(3) UNSIGNED DEFAULT 0,
-    FOREIGN KEY (Player) REFERENCES TeamRoster(ID) ON DELETE CASCADE
+CREATE TABLE Players (
+    userID INT(10) PRIMARY KEY,
+    gameTag VARCHAR(100),
+    rank VARCHAR(150) NOT NULL,
+    mmr INT,
+    FOREIGN KEY (userID) REFERENCES Users(userID)
 );
 
-INSERT INTO Statistics
-VALUES (17, 100, 35, 12, 47, 11, 21),
-       (18, 107, 13, 22, 13, 1, 3),
-       (19, 111, 10, 0, 18, 2, 4),
-       (20, 128, 2, 45, 9, 1, 2),
-       (21, 107, 15, 39, 26, 3, 7),
-       (22, 100, 29, 47, 27, 9, 8);    
+INSERT INTO Players
+VALUES 
+(1000, 'ShadowReaper', 'Gold', 1450),
+(1003, 'NovaStrike', 'Platinum', 1820)
+
+CREATE TABLE Coachs (
+    userID INT PRIMARY KEY,
+    experience_years INT,
+    certification VARCHAR(150),
+    FOREIGN KEY (userID) REFERENCES Users(userID)
+);
+
+INSERT INTO Coachs 
+VALUES 
+(1001, 10, 'Level A Coaching License');
+
+CREATE TABLE Teams (
+    userID INT,
+    teamID INT,
+    teamname VARCHAR(100),
+    role_in_team ENUM('Captain', 'Leader', 'Support', 'Sniper', 'Member'，'Coach','Sub')
+    status VARCHAR(50),
+    scheduleID INT,
+    PRIMARY KEY (userID, teamID),
+    FOREIGN KEY (userID) REFERENCES Users(userID),
+    FOREIGN KEY (teamID) REFERENCES TeamInfo(teamID)
+);
+
+INSERT INTO Teams 
+VALUES
+(1001, T101, 'DragonSlayers', 'Captain', 'Active', S1001),
+(1003, T101, 'DragonSlayers', 'Support', 'Active', S1001),
+(1002, T101, 'DragonSlayers', 'Coach', 'active', S1001),
+
+CREATE TABLE Games (
+    gameID INT PRIMARY KEY,
+    gameType VARCHAR(100),
+    gameName VARCHAR(150),
+    gameSeason VARCHAR(50),
+    gameMatch VARCHAR(100),
+    gameRound VARCHAR(50),
+    game_start_date DATE,
+    game_end_date DATE
+);
+
+INSERT INTO Games (gameID, gameType, gameName, gameSeason, gameMatch, gameRound, game_start_date, game_end_date)
+VALUES 
+(G00001, 'FPS', 'Valorant Championship', '2025', 'Match 1', 'Round 1', '2025-06-01', '2025-06-01');
+
+CREATE TABLE Schedule (
+    scheduleID INT PRIMARY KEY,
+    gameID INT NOT NULL,
+    gameDate DATETIME NOT NULL,
+    teamID INT NOT NULL,
+    status ENUM('Scheduled', 'Completed', 'Cancelled', 'Postponed') NOT NULL,
+    description VARCHAR(255)
+);
+
+INSERT INTO Schedule (scheduleID, gameID, gameDate, teamID, status, description)
+VALUES 
+(S1010, G00010, '2026-05-01 15:00:00', 10, 'Scheduled', 'Opening match'),
+(s1011, G00012, '2026-05-02 18:30:00', 12, 'Scheduled', 'Evening game')
+
+CREATE TABLE Records (
+    gameID INT NOT NULL,
+    playerID INT NOT NULL,
+    points INT DEFAULT 0,
+    PRIMARY KEY (gameID, playerID),
+    CONSTRAINT fk_records_game
+        FOREIGN KEY (gameID) REFERENCES Game(gameID),
+    CONSTRAINT fk_records_player
+        FOREIGN KEY (playerID) REFERENCES Player(playerID)
+);
+
+INSERT INTO Records (gameID, playerID, points)
+VALUES
+(G10001, 10001, 25),
+
